2d50375a175320f3b5f490f4894bcc091f2c7600
Olutsha <smolutsha@gmail.com>
2023-08-16 13:29:03 +0200

    first commit

---
+++ totalPhoneBill.js
@@ -0,0 +1,15 @@
+
+export  function totalPhoneBill(callsAndSms){
+  let totalCost = 0;
+  callsAndSms.split(",").forEach((item) => {
+    item = item.trim();
+    if (item === "call") {
+      totalCost += 2.75;
+    } else if (item === "sms") {
+      totalCost += 0.65;
+    }
+  });
+  return totalCost.toFixed(2);
+};
+
+// expor   = {totalPhoneBill}
